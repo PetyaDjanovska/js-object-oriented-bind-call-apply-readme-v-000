df36bf5bbf7cc9677cdbb611fed501b284f780e1
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-25 17:14:37 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -61,11 +61,11 @@ class User {
     }
 }
 
-let billy = new User('billy', ['music', 'art', 'movies']);
-let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
-
 // matchInterests(event) {
 //   return event.keywords.some(word => this.interests.includes(word));
 // }
 
+
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
 billy.matchInterests(freeMusic);
