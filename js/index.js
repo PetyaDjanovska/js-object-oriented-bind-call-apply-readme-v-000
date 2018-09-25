3365b7522d15eff23dea2ca76527f274d2969dfb
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-25 16:53:17 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -2,12 +2,19 @@
 function greet() {
     console.log(`my name is ${this.name}, hi!`);
 }
- 
+
 greet(); // my name is , hi!
- 
+
 let person = {
     name: 'Bob',
     greet: greet
 };
- 
+
 person.greet();
+let sally = { name: 'Sally' };
+
+greet.call(sally);
+// my name is Sally, hi!
+
+greet.apply(sally);
+// my name is Sally, hi!
